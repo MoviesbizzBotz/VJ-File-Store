67407b33070df3a8988cde2a161ae52344168eb6
MoviesbizzBotz <91929263+MoviesbizzBotz@users.noreply.github.com>
2025-01-14 16:43:15 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,9 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    
+  CAPTION = """<b>{file_name}
+
+{file_caption}</b>"""  
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
